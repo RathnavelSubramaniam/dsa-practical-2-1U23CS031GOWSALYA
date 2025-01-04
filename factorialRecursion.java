b3a51eb5c0687153313af777407b5d4813ebcd85
1U23CS031GOWSALYA <139536856+1U23CS031GOWSALYA@users.noreply.github.com>
2025-01-04 04:25:45 +0000

    P2

--- factorialRecursion.java
+++ factorialRecursion.java
@@ -1,6 +1,8 @@
 import java.util.*;
-public class factorialRecursion{
-public static void main(String[] args){
+public class factorialRecursion
+{
+public static void main(String[] args)
+{
 System.out.println("PROGRAM TO FIND THE FACTORIAL OF A GIVEN NUMBER USING RECURSION");
 System.out.println("ENTER THE NUMBER TO FIND THE FACTORIAL OF A GIVEN NUMBER USING RECURSION");
 Scanner in = new Scanner(System.in);
@@ -14,9 +16,14 @@ System.out.println("FACTORIAL OF A GIVEN NUMBER IS: " +factorial(n));
 }
 static int factorial(int n)
 {
+if(n==0||n==1)
+{
+  return 1;
+}
+else
+{
+  return n*factorial(n-1);
 
-
-//Start your Code Here
-
+}
 }
 }
